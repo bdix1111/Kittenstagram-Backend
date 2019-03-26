cd43e1c3cf8dc6685c8a846269ae80b302ffd668
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2019-03-26 11:57:54 -0400

    added like functionality

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,9 +1,18 @@
 class Api::V1::LikesController < ApplicationController
 
   def index
-    @likes = Like.all 
+    @likes = Like.all
 
     render json: @likes
+  end
+
+
+
+  def create
+
+    @like = Like.create(post_id: params['post_id'])
+    # byebug
+    render json: @like
   end
 
 end

2d55304d48397a6d02d95d57f77110f9438d1b39
bdix1111 <brianna_dixon@rocketmail.com>
2019-03-26 16:10:20 -0400

    add comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -3,7 +3,13 @@ class Api::V1::CommentsController < ApplicationController
   def index
     @comments = Comment.all
 
-    render json: @comments 
+    render json: @comments
+  end
+
+  def create
+    @comment = Comment.create(content: params['content'], post_id: params['post_id'])
+
+    render json: @comment
   end
 
 end
